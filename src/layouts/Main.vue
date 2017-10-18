fee7c2b28872faed6ea435bef9adb02793a4484e
JoryOne <1442933378@qq.com>
2017-10-18 23:03:29 +0800

    add vue

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -2,11 +2,20 @@
   <div class="container">
     <ul>
       <li>
-        <v-link href="/">Home</v-link>
-        <v-link href="/about">About</v-link>
+        <v-link href="/"><md-button href="/">Add</md-button></v-link>
+        <v-link href="/about"><md-button href="/">Addd</md-button></v-link>
       </li>
     </ul>
+    <md-toolbar md-theme="orange">
+  <md-button class="md-icon-button">
+    <md-icon>menu</md-icon>
+  </md-button>
 
+  <h2 class="md-title" style="flex: 1">Vue Material</h2>
+
+  <md-button href="/">Add</md-button>
+  <md-button href="/about">Remove</md-button>
+</md-toolbar>
     <slot></slot>
   </div>
 </template>
